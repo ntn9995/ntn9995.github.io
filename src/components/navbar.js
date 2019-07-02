92c5d21d9b3fe1bd4f60cc5bebc7f9dfd09801d4
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-07-01 23:11:24 -0400

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,35 @@
 import React from "react"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {Link} from "gatsby"
+import {faGithub, faLinkedin, faMedium} from "@fortawesome/free-brands-svg-icons"
+import {faHome, faBookOpen} from "@fortawesome/free-solid-svg-icons"
 import NavbarContainer from "./css_modules/navbar-container.module.css"
+
+const github = <FontAwesomeIcon icon={faGithub}/>
+const linkedin = <FontAwesomeIcon icon={faLinkedin}/>
+const medium = <FontAwesomeIcon icon={faMedium}/>
+const home = <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+const blog = <FontAwesomeIcon icon={faBookOpen}></FontAwesomeIcon>
 
 export default () => (
     <div className={NavbarContainer.navbarContainer}>
         <div className={NavbarContainer.navLink}>
-            Home
+            <Link to="/">{home}</Link>
         </div>
         <div className={NavbarContainer.navLink}>
-            Blog
+            <Link to="/posts/">{blog}</Link>
         </div>
         <div className={NavbarContainer.navLink}>
             
         </div>
         <div className={NavbarContainer.navLink}>
-            github
+            <a href="https://github.com/ntn9995" title="github">{github}</a>
         </div>
         <div className={NavbarContainer.navLink}>
-            linkedin
+            <a href="https://www.linkedin.com/in/ngoc-nguyen-630402b9/" title="linkedin">{linkedin}</a>
         </div>
         <div className={NavbarContainer.navLink}>
-            medium
+            <a href="https://medium.com/@ngocnguyen9995" title="medium">{medium}</a>
         </div>
     </div>
 )
