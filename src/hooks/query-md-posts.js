ca1add3f4e03dce905bb10affe7d78071108ac16
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-07-02 16:39:43 -0400

    add markdown id to query

--- src/hooks/query-md-posts.js
+++ src/hooks/query-md-posts.js
@@ -13,6 +13,7 @@ export const queryAllPostsSummary = () => {
                         date
                       }
                       excerpt
+                      id
                     }
                   }
                 }
