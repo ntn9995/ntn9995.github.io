ae83b4a11939b5f43b8909fd92e1c9ae7f9a167f
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-07-02 16:02:35 -0400

    added staticquery hook for querying blogposts

---
+++ src/hooks/query-md-posts.js
@@ -0,0 +1,24 @@
+import { useStaticQuery, graphql} from "gatsby"
+
+export const queryAllPostsSummary = () => {
+    const { data } = useStaticQuery(
+        graphql`
+            query {
+                allMarkdownRemark(filter: {frontmatter: {category: {eq: "blog"}}}) {
+                  edges {
+                    node {
+                      frontmatter {
+                        title
+                        tags
+                        date
+                      }
+                      excerpt
+                    }
+                  }
+                }
+              }
+        `
+    )
+
+    return data
+}
