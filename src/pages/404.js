0adc814dbd89da584186fdcd64b67746a0f6a8b6
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-07-03 03:04:35 -0400

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+import { layer } from "@fortawesome/fontawesome-svg-core";
+
+export default () => (
+    <Layout>
+        <div style= {{
+            fontSize: "3.2rem",
+            margin: "3.2rem 0",
+            textAlign: "center"
+        }}>
+            <h1>
+                404 !!! （╯°□°）╯︵( .o.)
+            </h1>
+        </div>
+    </Layout>
+)
