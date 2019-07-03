646e3631355171fe5bff4e25df83477704f657d8
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-07-03 01:05:58 -0400

    link to posts and 2 more posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { usePosts } from "../hooks/use-posts"
+import { Link } from "gatsby"
 import BlogListStyle from "../components/css_modules/blog-list.module.css"
 
 export default () => {
@@ -12,17 +13,20 @@ export default () => {
                 <h1>Posts</h1>
                 {edges.map(({node}) => (
                     <div key={node.id}>
-                        <div className={BlogListStyle.blogList}>
-                            <div className={BlogListStyle.metadata}>
-                                <h3>{node.frontmatter.title}</h3>
+                            <div className={BlogListStyle.blogList}>
+                                <Link to={node.fields.slug}>
+                                    <div className={BlogListStyle.metadata}>
+                                        <h3>{node.frontmatter.title}</h3>
+                                    </div>
+                                </Link>
+                                <div className={BlogListStyle.metadata}>
+                                    <div className={BlogListStyle.tag}>{node.frontmatter.tags[0]}</div>
+                                </div>
+                                <div className={BlogListStyle.metadata}>
+                                    <div className={BlogListStyle.date}>{node.frontmatter.date}</div>
+                                </div>
                             </div>
-                            <div className={BlogListStyle.metadata}>
-                                <div className={BlogListStyle.tag}>{node.frontmatter.tags[0]}</div>
-                            </div>
-                            <div className={BlogListStyle.metadata}>
-                                <div className={BlogListStyle.date}>{node.frontmatter.date}</div>
-                            </div>
-                        </div>
+                        
                         <div className={BlogListStyle.excerpt}>
                             <p>
                                 {node.excerpt}
